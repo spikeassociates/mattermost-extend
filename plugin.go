349d90d559247e25bf708793d7f7b60ab78a9591
Kliv Shapllo <k.shapllo@studiosynthesis.biz>
2019-01-15 17:37:58 +0100

    MM hook to capture post and launch event"

--- plugin.go
+++ plugin.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+	"regexp"
+)
+
+type MMPlugin struct {
+	plugin.MattermostPlugin
+}
 
 func main() {
-	fmt.Println("working")
+	plugin.ClientMain(&MMPlugin{})
+}
+
+func (p *MMPlugin) MessageHasBeenPosted(c *plugin.Context, post *model.Post) {
+
+	r, _ := regexp.Compile("^#(show|edit|view|create) (\\w+)$")
+	matches := r.FindStringSubmatch(post.Message)
+
+	if len(matches) > 0 {
+		action := matches[1]
+		parameter := matches[2]
+
+		broadcast := &model.WebsocketBroadcast{UserId: post.UserId}
+
+		payloadData := map[string]interface{}{
+			"action":    action,
+			"parameter": parameter,
+		}
+		p.API.PublishWebSocketEvent("corebos", payloadData, broadcast)
+	}
+
 }
